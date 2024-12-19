396ae41f1e3b7eff59481c96f7082cde3ed88fc8
nstienen <n.l.stienen@student.tudelft.nl>
2024-12-19 11:47:20 +0100

    Updated file name convention and overlapped identical setups

--- measurements_analysis.m
+++ measurements_analysis.m
@@ -1,13 +1,14 @@
 clear
 clc
 
-measurements = load("Measurements_Session_1\AllData.mat");
+% measurements = load("Measurements_Session_1\AllData.mat");
+measurements = load("Measurements_Session_2\AllData.mat");
 m = measurements.dataStruct;
 fields = fieldnames(m); % Get all field names of the structure
 
 
 figure(1); clf; % Create a new figure for each plot
-tiledlayout(3,4)
+tiledlayout(4,4)
 for i = 1:length(fields)
     currentField = fields{i}; % Get the current field name
     if isfield(m.(currentField), 'data') % Check if 'data' field exists
@@ -22,6 +23,7 @@ for i = 1:length(fields)
         title(['Plot for ', m.(currentField).fileName]);
         xlabel('Displacement');
         ylabel('Force');
+        grid on
     else
         disp(['Field ', currentField, ' does not have a "data" sub-structure.']);
     end
---
+++ measurements_analysis_session2.m
@@ -0,0 +1,53 @@
+clear
+clc
+
+% measurements = load("Measurements_Session_1\AllData.mat");
+measurements = load("Measurements_Session_2\AllData.mat");
+m = measurements.dataStruct;
+fields = fieldnames(m); % Get all field names of the structure
+
+
+figure(1); clf; % Create a new figure for each plot
+tiledlayout(2,3)
+for i = 1:length(fields)
+    currentField = fields{i}; % Get the current field name
+    if isfield(m.(currentField), 'data') % Check if 'data' field exists
+        dataStruct = m.(currentField).data;
+        % Extract data for plotting
+        displacement = dataStruct.Displacement_Plot0;
+        force = dataStruct.Force_Plot0;
+        
+        % Plot the data
+        if endsWith(m.(currentField).fileName, '1')
+            nexttile;
+            hold on
+        end
+
+        % Extract relevant components from the fileName
+        tokens = regexp(m.(currentField).fileName, 'f(\d{2}) (\d{2}) d(\d{2})mm s(\d) r(\d)', 'tokens', 'once');
+        flexureXX = tokens{1};
+        flexureYY = tokens{2};
+        displacementZZ = tokens{3};
+        spring = tokens{4};
+        run = tokens{5};
+
+        plot(displacement, force, DisplayName=['Run ', run]);
+        if spring == '1'
+            title(['Flexures ', flexureXX, '-', flexureYY, '-', flexureXX, ', and displacement ', displacementZZ, 'mm, with spring']);
+        else
+            title(['Flexures ', flexureXX, '-', flexureYY, '-', flexureXX, ', and displacement ', displacementZZ, 'mm']);
+        end
+        xlabel('Displacement');
+        ylabel('Force');
+        grid on
+        xlim([0,round(max(5*displacement))/5])
+        if flexureXX == '20'
+            ylim([0,30])
+        else
+            ylim([0,20])
+        end
+        legend(Location="southeast")
+    else
+        disp(['Field ', currentField, ' does not have a "data" sub-structure.']);
+    end
+end
